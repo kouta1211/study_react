f66dd188733e4b7f25d2747f59466a8e75b9d4fa
kouta matsumoto <masutema1211@gmail.com>
2026-02-02 17:26:33 +0900

    ♻️Footer部分とText部分をコンポーネント化

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="mt-16 border-t py-6 text-center text-sm text-zinc-500">
+            © 2026 My App
+        </footer>
+    )
+}
