599ba1756100733b08e20df8b443720dffb9fb2b
kouta matsumoto <masutema1211@gmail.com>
2026-02-02 17:35:56 +0900

    🔥indexフォルダの消去とその中身をデフォルトにした

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
 import Image from "next/image";
+import Footer from "@/components/Footer";
+import Text from "@/components/text";
+
+export const metadata: Metadata = {
+  title: "Index",
+  description: "Index Pageです",
+}
 
 export default function Home() {
   return (
@@ -14,25 +22,9 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Index Page
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          <Text />
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
@@ -60,6 +52,7 @@ export default function Home() {
           </a>
         </div>
       </main>
+      <Footer />
     </div>
   );
 }
