a69d98fbe9f15e65720e98779eb661262261234c
kouta matsumoto <masutema1211@gmail.com>
2026-02-05 01:05:29 +0900

    ♻️コンポーネントの細分化とレイアウトの調節

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
+import { Action } from "@/components/Action";
+import { Headline } from "@/components/Headline";
 import { Metadata } from "next";
-import {Headline} from "@/components/Headline";
+import { Main } from "@/components/Main";
 
 export const metadata: Metadata = {
-  title: "Index",
-  description: "Index Pageです",
-}
+	title: "Index",
+	description: "Index Pageです",
+};
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Headline title="Index" />
-    </div>
-  );
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+				<Headline />
+				<Main title="Index" />
+				<Action />
+			</main>
+		</div>
+	);
 }
